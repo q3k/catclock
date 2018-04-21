c83f8dc05070253290b8ea59a6e25cb370aa5d97
Sergiusz Bazanski <q3k@q3k.org>
2018-04-21 16:14:57 +0100

    Reorder includes, they're signifact in Plan9...

--- catclock.c
+++ catclock.c
@@ -1,8 +1,8 @@
 /* Copyright 1985 Massachusetts Institute of Technology */
+#include <u.h>
+#include <libc.h>
 #include <draw.h>
 #include <event.h>
-#include <libc.h>
-#include <u.h>
 
 #define add addpt
 #define sub subpt
